ed9e1707a834b3d992b8972c98de17a3b9d71b2d
3ndrius <andrzejo1991@gmail.com>
2020-08-17 14:27:36 +0200

    Adding post controller

--- server/src/controller/bookController.js
+++ server/src/controller/bookController.js
@@ -1,22 +1,37 @@
-const express = require('express');
+const express = require("express");
 const bookController = express.Router();
-const Book = require('../models/book.model');
+const Book = require("../models/book.model");
 
-/* Get all Posts */
-bookController.get('/', (req, res, next) => {
-    Book.find({} , function(err, result){
-        if(err){
-            res.status(400).send({
-                'success': false,
-                'error': err.message
-            });
-        }
-        res.status(200).send({
-            'success': true,
-            'data': result
-        });
+/* Get all saved books */
+bookController.get("/", async (req, res) => {
+  try {
+    const books = await Book.find({});
+    res.status(200).json({ success: true, books });
+  } catch (e) {
+    res.status(500).json({
+      success: false,
+      error: "Cannot get books form server!: " + e,
     });
+  }
 });
 
+bookController.post("/", async (req, res) => {
+  try {
+    let book = await Book.create(req.body);
+    res.json({
+      success: true,
+      book,
+    });
+  } catch (e) {
+    res
+      .staus(500)
+      .send({ 
+        success: false,
+        error: "Cannot add book sth wrong!: " + e 
+      });
+  }
+});
+
+bookController.post("/search", async (req, res) => {});
 
 module.exports = bookController;
