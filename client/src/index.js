2b18255b5c7c1468a7bda1ddc0be3bc15e9c993f
3ndrius <andrzejo1991@gmail.com>
2020-08-18 09:49:24 +0200

    Config redux, react-redux, redux-saga

--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
+import { BrowserRouter as Router } from "react-router-dom";
+// store
+import createSagaMiddleware from "redux-saga";
+import { createStore, applyMiddleware, compose } from "redux";
+import { Provider } from "react-redux";
+
+// imports
+import reducer from "./redux/reducers";
+import saga from "./sagas";
+
+const sagaMiddleware = createSagaMiddleware();
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(sagaMiddleware),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
+
+sagaMiddleware.run(saga); //saga
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <Router>
+        <App />
+      </Router>
+    </Provider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want your app to work offline and load faster, you can change
---
+++ client/src/sagas/index.js
@@ -0,0 +1,10 @@
+import { call, put, takeEvery, all, delay } from "redux-saga/effects";
+
+
+
+
+export default function* rootSaga() {
+  yield all([
+   
+  ]);
+}
