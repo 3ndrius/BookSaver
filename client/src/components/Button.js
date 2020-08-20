572cd144a49413e4ab4f4e4cfd61e62b9c264a7a
3ndrius <andrzejo1991@gmail.com>
2020-08-20 10:26:40 +0200

    Change btn color

--- client/src/components/Button.js
+++ client/src/components/Button.js
@@ -2,12 +2,12 @@ import styled from "styled-components";
 
 export const Button = styled.button`
   /* Adapt the colors based on primary prop */
-  background: ${(props) => (props.primary ? "palevioletred" : "white")};
-  color: ${(props) => (props.primary ? "white" : "palevioletred")};
+  background: ${(props) => (props.primary ? ({theme}) => theme.dark200 : ({theme}) => theme.white100)};
+  color: ${(props) => (props.primary ? ({theme}) => theme.white100 : ({theme}) => theme.dark200)};
   width: ${(props) => (props.large ? "130px" : " 120px")};
   height: 45px;
   font-size: 1em;
-  border: 2px solid palevioletred;
+  border: 2px solid ${({theme}) => theme.dark300};
   border-radius: 1px;
   cursor: pointer;
 `;
